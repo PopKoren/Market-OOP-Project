905b318e89b88bdc88ad440562a519e554f6a175
PopKoren <korenabugov@gmail.com>
2023-05-09 13:43:06 +0300

    Add files via upload

---
+++ eliran/Bursa/Bursa/Backend/Models/BTCchain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bursa.Backend.Models
+{
+    public class BTCchain : Crypto
+    {
+
+        protected BTCchain(string name, int price, bool mineable, bool stable, int totalsup)
+             : base(name, price, mineable, stable, totalsup)
+        {
+
+        }
+    }
+}

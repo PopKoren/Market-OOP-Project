817617aa0347789c4b140c92b625c7a2df208a9f
PopKoren <korenabugov@gmail.com>
2023-05-13 19:48:26 +0300

    Add files via upload

---
+++ OOP-Project-main/OOP-Project-main/Project/Bursa/Bursa/Backend/Models/Stock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bursa.Backend.Models
+{
+    public abstract class Stock : Item
+    {
+        protected Stock(string name, int price)
+           : base(name, price)
+        {
+          
+        }
+    }
+}
---
+++ OOP-Project-main/OOP-Project-main/Project/Bursa/Bursa/Frontend/Forms/RegisterLogin.cs
@@ -0,0 +1,25 @@
+﻿using Bursa.Frontend.UserControls;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bursa.Frontend.Forms
+{
+    public partial class RegisterLogin : Form
+    {
+        public RegisterLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+            MainForm mainf = new MainForm(true);
+            mainf.Show();
+        }
+
+     
+    }
+}

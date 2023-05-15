24fbd9250194ed62889b87cf76a79764552ecd5a
PopKoren <korenabugov@gmail.com>
2023-05-15 23:03:31 +0300

    Add files via upload

---
+++ Bursa/Bursa/Backend/Models/Tech.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bursa.Backend.Models
+{
+    public class Tech : Stock
+    {
+        protected Tech(string name, int price)
+           : base(name, price)
+        {
+
+        }
+    }
+}
---
+++ Bursa/Bursa/Backend/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bursa.Backend.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime Dateofbirth { get; set; }
+        public string Email { get; set; }
+        public double Balance { get; set; }
+
+        public User(string username, string password, DateTime dateofbirth, string email, double balance = 0)
+        {
+            Username = username;
+            Password = password;
+            Dateofbirth = dateofbirth;
+            Email = email;
+            Balance = balance;
+        }
+
+
+    }
+}

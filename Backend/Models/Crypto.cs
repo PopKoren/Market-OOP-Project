fb73ac88feb47e523ce8111f8f3fa5c89a7507b1
PopKoren <korenabugov@gmail.com>
2023-05-31 19:20:18 +0300

    Add files via upload

---
+++ Backend/Models/Crypto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bursa.Backend.Models
+{
+    [Serializable]
+
+    public abstract class Crypto : Item
+    {
+        public bool IsMinable { get; set; }
+
+        public bool IsStableCoin { get; set; }
+
+
+        public Crypto(string name, int quantity, int price, bool mineable, bool stable)
+            : base(name, quantity, price)
+        {
+            IsMinable = mineable;
+            IsStableCoin = stable;
+        }
+        public abstract override Image GetPicture();
+
+    }
+}
---
+++ Backend/Models/Stock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bursa.Backend.Models
+{
+    [Serializable]
+
+    public abstract class Stock : Item
+    {
+        public abstract override Image GetPicture();
+
+        public Stock(string name, int quantity, int price)
+           : base(name, quantity, price)
+        {
+          
+        }
+    }
+}
